fb0460dc0b6d9b7df283682cc36ec279a50a0a04
sureshshetty354 <sureshshetty354@gmail.com>
2026-07-29 23:43:40 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,19 @@
+# Main Terraform Configuration
 
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+# AWS Provider Configuration
+
+provider "aws" {
+  region = var.aws_region
+}
