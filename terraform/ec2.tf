7da4f702a1f613f34e51dd1f3282d0bf559f412f
sureshshetty354 <sureshshetty354@gmail.com>
2026-07-29 23:13:27 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,1 +1,16 @@
+resource "aws_instance" "jenkins" {
 
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  subnet_id = aws_subnet.public.id
+
+  vpc_security_group_ids = [
+    aws_security_group.automation_sg.id
+  ]
+
+  tags = {
+    Name = "Jenkins-Server"
+  }
+}
