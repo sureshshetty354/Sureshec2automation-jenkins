9aa04caf5ae070115595ab96376912cd3a3f177e
sureshshetty354 <sureshshetty354@gmail.com>
2026-07-29 23:14:10 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,11 @@
-
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+output "jenkins_public_ip" {
+  value = aws_instance.jenkins.public_ip
 }
 
-provider "aws" {
-  region = var.aws_region
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.public.id
 }
