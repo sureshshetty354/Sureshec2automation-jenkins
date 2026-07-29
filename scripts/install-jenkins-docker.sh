ba7981b35b9e7ea88d6ffc53319d2c12929f7f70
sureshshetty354 <sureshshetty354@gmail.com>
2026-07-29 23:19:14 +0530

    Create install-jenkins-docker.sh

---
+++ scripts/install-jenkins-docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Update server
+yum update -y
+
+# Install Java (required for Jenkins)
+yum install java-17-amazon-corretto -y
+
+# Install Git
+yum install git -y
+
+# Install Docker
+yum install docker -y
+
+systemctl start docker
+systemctl enable docker
+
+# Jenkins Installation
+wget -O /etc/yum.repos.d/jenkins.repo \
+https://pkg.jenkins.io/redhat-stable/jenkins.repo
+
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+yum install jenkins -y
+
+systemctl start jenkins
+systemctl enable jenkins
+
+# Give Jenkins permission to use Docker
+usermod -aG docker jenkins
+
+systemctl restart jenkins
+
+echo "Jenkins and Docker installation completed"
